1920ac9dc492f46d1032720fb28741f07760d5aa
kyranf <kyfindlater@gmail.com>
2016-04-12 07:40:44 -0700

    Added files via upload
    initial commit.

---
+++ robotarmy_0.1.0/data.lua
@@ -0,0 +1,3 @@
+require("prototypes.droiditem") -- any buildable or placable object/entity needs this 
+require("prototypes.droidentity") -- any buildable or placable object/entity needs this
+require("prototypes.droidrecipe")
---
+++ robotarmy_0.1.0/prototypes/droiditem.lua
@@ -0,0 +1,13 @@
+data:extend(
+{
+  {
+    type = "item",
+    name = "droid",
+    icon = "__base__/graphics/icons/construction-robot.png",
+    flags = {"goes-to-quickbar"},
+    order = "z[not-used]",
+    place_result = "droid",
+    stack_size = 25
+  }
+}
+)
