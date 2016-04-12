5ef7ae451271393a6c6285b6db8a96ed31180f32
kyranf <kyfindlater@gmail.com>
2016-04-12 13:47:29 -0700

    added explosion on death. new icon for droid item.

---
+++ robotarmy_0.1.1/prototypes/droiditem.lua
@@ -0,0 +1,14 @@
+data:extend(
+{
+  {
+    type = "item",
+    name = "droid",
+    icon = "__robotarmy__/graphics/icons/droid.png",
+    flags = {"goes-to-quickbar"},
+    order = "z[droid]",
+	subgrou = "capsule",
+    place_result = "droid",
+    stack_size = 25
+  }
+}
+)
