52d8dc74378efafd827b0a87b8d75bd6d0b4bbde
Kyran Findlater <kyfindlater@gmail.com>
2016-04-22 22:34:52 -0700

    Removing old files

--- robotarmy_0.1.1/data.lua
+++
@@ -1,3 +0,0 @@
-require("prototypes.droiditem") -- any buildable or placable object/entity needs this 
-require("prototypes.droidentity") -- any buildable or placable object/entity needs this
-require("prototypes.droidrecipe")
--- robotarmy_0.1.2/data.lua
+++
@@ -1,4 +0,0 @@
-require("prototypes.droiditem") -- any buildable or placable object/entity needs this 
-require("prototypes.droidentity") -- any buildable or placable object/entity needs this
-require("prototypes.droidrecipe")
-require("lib.30log")
