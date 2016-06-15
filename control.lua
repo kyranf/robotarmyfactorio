2a669b3880c5ebd2794ef17de630d0e21d877950
Kyran Findlater <kyfindlater@gmail.com>
2016-06-14 18:23:16 -0700

    fixed recipe tech unlocks and migration script format

--- control.lua
+++ control.lua
@@ -231,6 +231,12 @@ script.on_load( function()
 	if not global.uniqueSquadId then
 		global.uniqueSquadId = {init_state = "ready"}
 	end
+	
+	if not global.droidCounters then
+		global.droidCounters = {}
+	end
+	
+	
 	global.runonce = false
 		
 end)
@@ -414,6 +420,34 @@ script.on_event(defines.events.on_tick, function( event)
 		for i, force in pairs(game.forces) do 
 			force.reset_technologies()
 		end
+		
+		
+		for _,force in pairs(game.forces) do
+				force.reset_recipes()
+				force.reset_technologies()
+
+				--Tech Additions for droids and droid counter combinator
+				if force.technologies["military"].researched then
+					force.recipes["droid-rifle"].enabled=true
+					force.recipes["droid-rifle-deploy"].enabled=true
+				end
+
+				if force.technologies["electronics"].researched then
+					force.recipes["droid-counter"].enabled=true
+				end
+
+				if force.technologies["military-2"].researched then
+					force.recipes["droid-smg"].enabled=true
+					force.recipes["droid-smg-deploy"].enabled=true
+					force.recipes["droid-rocket"].enabled=true
+					force.recipes["droid-rocket-deploy"].enabled=true
+				end
+			  
+				if force.technologies["military-3"].researched then
+					force.recipes["terminator"].enabled=true
+					force.recipes["terminator-deploy"].enabled=true
+				end
+		end     
 
 		global.runonce = true
 	end
--- migrations/robotarmy_0.1.4.lua
+++ migrations/robotarmy_0.1.4.lua
@@ -1,21 +1,28 @@
--- need to put transition from "droid" to "droid-smg" here.
-
-
 game.reload_script()
 
 for i, force in pairs(game.forces) do 
- force.reset_recipes()
-end
+	force.reset_recipes()
+	force.reset_technologies()
+ --Tech Additions for droids and droid counter combinator
+	if force.technologies["military"].researched then
+		force.recipes["droid-rifle"].enabled=true
+		force.recipes["droid-rifle-deploy"].enabled=true
+	end
 
-for i, force in pairs(game.forces) do 
- force.reset_technologies()
-end
+	if force.technologies["electronics"].researched then
+		force.recipes["droid-counter"].enabled=true
+	end
 
-{
- "entity":
- [
-  ["droid", "droid-smg"],
-  ["droid-deploy", "droid-smg-deploy"]  
- ]
-}
+	if force.technologies["military-2"].researched then
+		force.recipes["droid-smg"].enabled=true
+		force.recipes["droid-smg-deploy"].enabled=true
+		force.recipes["droid-rocket"].enabled=true
+		force.recipes["droid-rocket-deploy"].enabled=true
+	end
+
+	if force.technologies["military-3"].researched then
+		force.recipes["terminator"].enabled=true
+		force.recipes["terminator-deploy"].enabled=true
+	end
+end
 
