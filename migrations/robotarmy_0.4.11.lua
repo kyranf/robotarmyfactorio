1be73369c89b7fb576fe48dc9583c5787243f9cd
snouz <snooz-x@hotmail.com>
2021-10-25 18:09:52 +0200

    (0.4.16) buildings, items order, icons

--- migrations/robotarmy_0.4.11.lua
+++ migrations/robotarmy_0.4.11.lua
@@ -9,47 +9,48 @@ for i, force in pairs(game.forces) do
 
     --force all of the known recipes to be enabled if the appropriate research is already done.
     if force.technologies["military"].researched then
-        force.recipes["droid-rifle"].enabled=true
-        force.recipes["droid-rifle-deploy"].enabled=true
+        force.recipes["droid-rifle"].enabled = true
+        force.recipes["droid-rifle-deploy"].enabled = true
         if(GRAB_ARTIFACTS == 1) then
-            force.recipes["loot-chest"].enabled=true
+            force.recipes["loot-chest"].enabled = true
         end
-        force.recipes["patrol-pole"].enabled=true
-        force.recipes["droid-guard-station"].enabled=true
-        force.recipes["droid-assembling-machine"].enabled=true
-        force.recipes["droid-pickup-tool"].enabled=true
-        force.recipes["droid-selection-tool"].enabled=true
-        force.recipes["droid-counter"].enabled=true
+        force.recipes["patrol-pole"].enabled = true
+        force.recipes["droid-guard-station"].enabled = true
+        force.recipes["droid-assembling-machine"].enabled = true
+        force.recipes["droid-pickup-tool"].enabled = true
+        force.recipes["droid-selection-tool"].enabled = true
+        force.recipes["droid-counter"].enabled = true
         force.recipes["droid-settings"].enabled = true
     end
 
 
     if force.technologies["military-2"].researched then
-        force.recipes["droid-smg"].enabled=true
-        force.recipes["droid-smg-deploy"].enabled=true
-        force.recipes["droid-rocket"].enabled=true
-        force.recipes["droid-rocket-deploy"].enabled=true
-        force.recipes["droid-flame"].enabled=true
-        force.recipes["droid-flame-deploy"].enabled=true
+        force.recipes["droid-smg"].enabled = true
+        force.recipes["droid-smg-deploy"].enabled = true
+        force.recipes["droid-rocket"].enabled = true
+        force.recipes["droid-rocket-deploy"].enabled = true
+        force.recipes["droid-flame"].enabled = true
+        force.recipes["droid-flame-deploy"].enabled = true
     end
 
     if force.technologies["military-3"].researched then
-        force.recipes["terminator"].enabled=true
-        force.recipes["terminator-deploy"].enabled=true
+        force.recipes["terminator"].enabled = true
+        force.recipes["terminator-deploy"].enabled = true
     end
 
     if force.technologies["defender"].researched then
-        force.recipes["defender-unit"].enabled=true
-        force.recipes["defender-unit-deploy"].enabled=true
+        force.recipes["defender-unit"].enabled = true
+        force.recipes["defender-unit-deploy"].enabled = true
     end
 
     if force.technologies["distractor"].researched then
-        force.recipes["distractor-unit"].enabled=true
-        force.recipes["distractor-unit-deploy"].enabled=true
+        force.recipes["distractor-unit"].enabled = true
+        force.recipes["distractor-unit-deploy"].enabled = true
     end
+
     if force.technologies["destroyer"].researched then
-        force.recipes["destroyer-unit"].enabled=true
-        force.recipes["destroyer-unit-deploy"].enabled=true
+        force.recipes["destroyer-unit"].enabled = true
+        force.recipes["destroyer-unit-deploy"].enabled = true
     end
 
 end
